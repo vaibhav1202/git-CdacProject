6967196afe5fd3816291fab24aaf92aa1871b8d9
Vaibhav Agrawal <42183016+vaibhav1202@users.noreply.github.com>
2021-09-22 02:05:16 +0530

    project

---
+++ src/main/java/com/text/doc/repositories/AdminRepository.java
@@ -0,0 +1,10 @@
+package com.text.doc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.text.doc.model.Admin;
+
+
+@Repository
+public interface AdminRepository extends JpaRepository<Admin, Long> {}
